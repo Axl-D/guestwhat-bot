08491abb69a56e9b420f1ecb70d1a169d1edce52
Axel Duheme <axel.duheme@gmail.com>
2024-02-19 22:07:38 +0100

    added reactions and updated the message

--- index.js
+++ index.js
@@ -8,15 +8,22 @@ const client = new Client({
 const channelId = process.env.CHANNEL_ID;
 const botToken = process.env.BOT_TOKEN;
 
+console.log(channelId, botToken);
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
   const channel = client.channels.cache.get(channelId);
   if (!channel) return console.error("Channel doesn't exist!");
   channel
-    .send("This is a test message, one minute from the script start.")
+    .send("Réunion Guestwhat aujourd'hui à 11:30 - confirmez votre participation avec un réaction à ce message ✅ ❌")
     .then((message) => {
       console.log(`Message sent successfully to ${message.channel.name}`);
+      message
+        .react("✅")
+        .then(() => message.react("❌"))
+        .then(() => console.log("Reactions added successfully"))
+        .catch((error) => console.error("Failed to add reaction:", error));
     })
     .catch((error) => {
       console.error(`Failed to send message: ${error}`);
