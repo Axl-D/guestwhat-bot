fad6d1f42d37e7de8e3b3f489f804fbcc322a6f0
Axel Duheme <axel.duheme@gmail.com>
2024-02-19 19:35:48 +0100

    Update code to use environment variables for sensitive information

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const client = new Client({
   intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
-const YOUR_CHANNEL_ID = "1070313793713885206";
-const BOT_TOKEN = "MTAzNzA2NTE1OTQ3MjA3MDc2Nw.GvEp7V.Trv9wmmpwRfbVoIMOb0iuJiGSETZI7pOi7iGUU";
+const channelId = process.env.CHANNEL_ID;
+const botToken = process.env.BOT_TOKEN;
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
-  const channel = client.channels.cache.get(YOUR_CHANNEL_ID);
+  const channel = client.channels.cache.get(channelId);
   if (!channel) return console.error("Channel doesn't exist!");
   channel
     .send("This is a test message, one minute from the script start.")
@@ -21,16 +21,6 @@ client.on("ready", () => {
     .catch((error) => {
       console.error(`Failed to send message: ${error}`);
     });
-  /*
-  // Schedule messages for Monday, Wednesday, and Friday at 9:00 AM
-  const scheduledMessage = new cron.CronJob("00 00 09 * * 1,3,5", () => {
-    const channel = client.channels.cache.get(YOUR_CHANNEL_ID);
-    if (!channel) return console.error("Channel doesn't exist!");
-    channel.send("Good morning! Don't forget our meeting today.");
-  });
-
-  scheduledMessage.start(); // Start the cron job
-  */
 });
 
-client.login(BOT_TOKEN);
+client.login(botToken);
